4fd5551a4d4fb67c7d58f6bef908ac0f89a2f41c
R1nBel <gleb.zamyakin@gmail.com>
2024-01-15 21:46:58 +0700

    pre_1

--- test/main.py
+++ test/main.py
@@ -28,7 +28,7 @@ while True:
 
 	img = cv2.flip(img, 1)
 
-	# cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+	#cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
 	cv2.imshow("Image", img)
 	cv2.waitKey(1)
